5837932c98800d017caf875e6f4c4cdb700851b5
Tom Crick <tomcrick@down-right.co.uk>
2015-05-01 11:50:51 +0100

    After Chaning The Model

--- app/controllers.js
+++ app/controllers.js
@@ -12,4 +12,15 @@ angular.module('myApp.controllers', []).controller("songCtrl", function($scope) 
   		artist: "Within Temptation",
   		title: "Ice Queen"
 	}];
+	$scope.newSong = { };
+
+	$scope.addSong = function(/** String */ artist, /** String */ title) {
+  		$scope.songs.push({
+    		artist: artist,
+    		title: title
+  			});
+  	$scope.newSong.title = "";
+  	$scope.newSong.artist = "";
+};
+
 });
